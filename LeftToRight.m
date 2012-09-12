4bc3a2716a6e375021d64d8f095e6458b4eac6c3
ezheng <ezheng@cs.unc.edu>
2012-09-12 14:11:16 -0400

    make a parallel version (using parfor)
    Signed-off-by: ezheng <ezheng@cs.unc.edu>

--- LeftToRight.m
+++ LeftToRight.m
@@ -23,25 +23,50 @@ for row = 1:h
     end
 end    
 % the remainning pixels
-for row = 1:h
-    row
+if(matlabpool('size') ==0)
+    matlabpool open;
+end
+
+localWindowSize = halfWindowSize;
+emptyMap = zeros(size(depthMap));
+tic;
+parfor row = 1:h          
+    emptyMap(row, :) = routine_LeftRight(randMap, image1_struct, image2_struct, depthMap, row, localWindowSize);    
+    fprintf('row %d is finished', row);
+end
+t = toc;
+fprintf(1, 'elapsed time is %f', t);
+depthMap = emptyMap;
+matlabpool close;
+
+end
+
+function oneRow = routine_LeftRight(randMap, image1_struct, image2_struct, depthMap, row, halfWindowSize)
+    [h,w,~] = size(image1_struct.imageData);
+%     oneRow = zeros(1,w);
     for col = halfWindowSize:w
-        col
-        start = col - halfWindowSize + 1;    
-%         propagate
+
+        start = col - halfWindowSize + 1;
+        %         propagate
         depthMap(row, col) = depthMap(row, col-1);
-%         cost of propagated depth
+        %         cost of propagated depth
         data1 = image1_struct.imageData(row, start:col);
         data2 = fetchColor( [start:col]', ones(halfWindowSize,1).* row, depthMap(row, start:col)',image1_struct, image2_struct );
         cost_1 = computeZNCC(data1, data2);
-%         cost of the rand map -----------------------------------------------   
+        %         cost of the rand map -----------------------------------------------
         depthMap(row, col) = randMap(row, col);
         data3 = fetchColor( [start:col]', ones(halfWindowSize,1).* row, depthMap(row, start:col)',image1_struct, image2_struct );
         cost_2 = computeZNCC(data1, data3);
         if(cost_2 < cost_1)
-            depthMap(row,col) = depthMap(row, col - 1);
-        end        
+        %   depthMap(row,col) = depthMap(row, col - 1);
+            depthMap(row, col) = depthMap(row,col-1);
+        else
+            depthMap(row,col) = randMap(row,col);
+        end
     end
+    oneRow = depthMap(row,:);
+    
 end
 
-end
+
+
