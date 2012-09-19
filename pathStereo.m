e348d6125ecea847a7e24f05665baabdce34283a
ezheng <ezheng@cs.unc.edu>
2012-09-19 00:57:41 -0400

    temp commit. This version has a bug.
    Signed-off-by: ezheng <ezheng@cs.unc.edu>

--- pathStereo.m
+++ pathStereo.m
@@ -11,13 +11,13 @@ image2 = double(imread(img2_struct.imageName));
 img1_struct.imageData = image1; [h, w, d] = size(image1); img1_struct.h = h; img1_struct.w = w; img1_struct.d = d;
 img2_struct.imageData = image2; [h, w, d] = size(image2); img2_struct.h = h; img2_struct.w = w; img2_struct.d = d;
 % ------------ verify camera poses
-% F = fundfromcameras(img1_struct.K * [img1_struct.R, img1_struct.T], img2_struct.K * [img2_struct.R, img2_struct.T]);
-% fig=vgg_gui_F(uint8(img2_struct.imageData), uint8(img1_struct.imageData),F);
+ F = fundfromcameras(img1_struct.K * [img1_struct.R, img1_struct.T], img2_struct.K * [img2_struct.R, img2_struct.T]);
+ fig=vgg_gui_F(uint8(img2_struct.imageData), uint8(img1_struct.imageData),F);
 
 % ------------
 
-% s = RandStream('mcg16807','Seed',0);
-% RandStream.setDefaultStream(s);
+ s = RandStream('mcg16807','Seed',0);
+ RandStream.setDefaultStream(s);
 
 depthMap = rand(h,w) * (far - near) + near; % depthMap initialization
 numOfIteration = 1;
